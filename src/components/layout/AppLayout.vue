838f85ee3988dc28ad5a39052bf74b10801b1bd4
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-09-26 23:04:06 +0800

    setup vue and vuetify

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <slot name="content" />
+        </v-container>
+      </v-main>
+      <v-footer></v-footer>
+    </v-app>
+  </v-responsive>
+</template>
