a582d904186f871a7743809a9c33ebdc34d5e128
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-09-27 17:49:27 +0800

    feat: added landing page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import NurseDashboardView from '@/views/system/NurseDashboardView.vue'
+import LandingPageView from '@/views/Guest/LandingPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'landing-page',
+      component: LandingPageView,
+    },
+    {
+      path: '/login',
       name: 'login',
       component: LoginView,
     },
