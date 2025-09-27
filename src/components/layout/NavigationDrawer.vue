697713cd161ff2c83955353962ba0606cd5b311f
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-09-27 10:33:13 +0800

    feat: set up navigation routes

--- src/components/layout/NavigationDrawer.vue
+++ src/components/layout/NavigationDrawer.vue
@@ -8,12 +8,40 @@ const drawer = computed({
   get: () => props.isDrawerOpen,
   set: (newVal) => emit('update:isDrawerOpen', newVal),
 })
+
+const routes = [
+  ['Dashboard', 'mdi-view-dashboard-outline', '/dashboard'],
+  ['Patients', 'mdi-account-multiple-outline', '/patients'],
+  ['Discharge Workflow', 'mdi-file-document-outline', '/workflow'],
+  ['Reports', 'mdi-chart-box-outline', '/reports'],
+]
 </script>
 
 <template>
   <v-navigation-drawer v-model="drawer">
+    <v-row>
+      <v-col cols="6" class="d-flex ga-2 ml-4 my-2">
+        <v-img width="50" src="/public/images/logo.png" rounded> </v-img>
+        <div>
+          <h3 class="text-h6">MediClear</h3>
+          <p class="text-caption">AHDMS</p>
+        </div></v-col
+      >
+    </v-row>
+
+    <v-divider></v-divider>
+
     <v-list>
-      <v-list-item title="Navigation drawer"></v-list-item>
+      <v-list-item
+        v-for="([title, icon, routes], i) in routes"
+        :key="i"
+        :prepend-icon="icon"
+        :title="title"
+        :value="title"
+        :to="routes"
+        ripple
+        color="blue-darken-2"
+      ></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
