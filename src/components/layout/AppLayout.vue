3e63e308ebed87fedf9badd8825daeb872522242
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-09-27 09:02:02 +0800

    feat: added navigation drawer

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,22 +1,38 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import NavigationDrawer from './NavigationDrawer.vue'
 
 const theme = ref('light')
 
 function onClick() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
+
+const isDrawerOpen = ref(false)
+
+onMounted(() => {
+  const savedDrawerState = localStorage.getItem('drawer')
+  if (savedDrawerState !== null) {
+    isDrawerOpen.value = savedDrawerState === 'true'
+  }
+})
+
+const toggleDrawer = () => {
+  isDrawerOpen.value = !isDrawerOpen.value
+  localStorage.setItem('drawer', isDrawerOpen.value)
+}
 </script>
 
 <template>
   <v-responsive class="border rounded">
     <v-app :theme="theme">
+      <NavigationDrawer v-model:isDrawerOpen="isDrawerOpen" />
       <v-app-bar class="px-3">
+        <v-app-bar-nav-icon @click="toggleDrawer" />
         <v-spacer></v-spacer>
 
         <v-btn
           :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
           slim
           @click="onClick"
         ></v-btn>
---
+++ src/components/layout/NavigationDrawer.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['isDrawerOpen'])
+const emit = defineEmits(['update:isDrawerOpen'])
+
+const drawer = computed({
+  get: () => props.isDrawerOpen,
+  set: (newVal) => emit('update:isDrawerOpen', newVal),
+})
+</script>
+
+<template>
+  <v-navigation-drawer v-model="drawer">
+    <v-list>
+      <v-list-item title="Navigation drawer"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
